3c7f0dbca8982a33732fae9351c21960e37579dd
Laith Soudani <laithsoudani@Laiths-MacBook-Pro.local>
2024-11-12 21:58:43 -0500

    Updated Lambda function and configurations

--- obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f33274337ab533d20a689aee4fd3d1276c8255")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
